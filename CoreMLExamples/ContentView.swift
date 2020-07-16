c3c5a39888ecfbe713b147b3a43c5ba317fedacb
Mamunul Mazid <mamunul.mazid@gmail.com>
2020-07-16 14:58:54 +0600

    Initial Commit

---
+++ CoreMLExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreMLExamples
+//
+//  Created by Mamunul Mazid on 16/7/20.
+//  Copyright © 2020 Mamunul Mazid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
